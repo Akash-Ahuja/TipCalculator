a4467d31520084fc34e6a371fd7f1a98a79c62bf
Akash Ahuja <42015413+Akash-Ahuja@users.noreply.github.com>
2021-01-15 01:57:13 -0500

    Add files via upload

---
+++ Prework_2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Prework_2_0Tests.swift
+//  Prework 2.0Tests
+//
+//  Created by Akash Ahuja on 1/14/21.
+//  Copyright © 2021 Akash Ahuja. All rights reserved.
+//
+
+import XCTest
+@testable import Prework_2_0
+
+class Prework_2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
